ed901bc30e83c150a5dbfb20e0161aebae48c727
Robert Lester <Lesters.robert@gmail.com>
2017-04-13 16:50:58 -0400

    added logic to get the table to display all data

---
+++ bamazonCustomer.js
@@ -0,0 +1,25 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    user: "root",
+
+    password: "root",
+    database: "Bamazon"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+});
+
+var run = function(){
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        console.log(res);
+    });
+};
+
+run();
